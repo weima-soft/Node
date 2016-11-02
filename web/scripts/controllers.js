f2b0daf74d13c0798799650c058f9feb579a7a05
WeimaSoft <alexandersu@126.com>
2016-11-02 20:21:23 +0800

    Finish UI

--- web/scripts/controllers.js
+++ web/scripts/controllers.js
@@ -1,18 +1,25 @@
 angular.module('App', []).controller('HelpItemController', function ($scope) {
 	$scope.ItemCollections = [];
-
-	for (var i = 0; i < 3; i++) {
+	var jIndex = 0;
+	var pageSize = 10;
+	
+	for (var i = 0; i < 4; i++) {
 		var itemCollection = {};
 		itemCollection.Id = "collection" + i;
 		itemCollection.CategoryName = "Category" + i;
 		itemCollection.Items = [];
-		for (var j = 0; j < 5 + i; j++) {
+		for (var j = jIndex; j < pageSize + jIndex; j++) {
 			var item = {};
 			item.Title = "Help Item Title " + j;
 			item.Content = "Help Item Content" + j;
 			item.Id = "ItemId" + j;
 			itemCollection.Items.push(item);
 		}
+		jIndex += pageSize;
 		$scope.ItemCollections.push(itemCollection);
 	}
+	
+	$scope.GdsTypes = ["Amadeus", "Galileo","Sabre"];
+	
+	$scope.OrderBy = ["ASC","DESC"];
 });
